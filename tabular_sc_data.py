c4cb99e54ffd54db0910804ac14c0e51693a9222
Randy Burgess <rb@ultiweb.com>
2020-05-02 05:19:52 -0400

    Added index column descriptions. Still have unwanted header in grand totals

--- tabular_sc_data.py
+++ tabular_sc_data.py
@@ -16,18 +16,23 @@ df = df.sort_index()
 df = df.loc[45001:45091, ['Combined_Key', 'Confirmed', 'Deaths', 'Last_Update']]
 df['Last_Update'] = pd.to_datetime(df['Last_Update']).dt.strftime('%m/%d/%Y %I:%M %p')
 
-df2 = df.sort_values(by=['Confirmed', 'Deaths'], ascending=False)
+df = df.sort_values(by=['Confirmed', 'Deaths'], ascending=False)
 
-df2['DeathRate'] = round(100 * (df2['Deaths'] / df2['Confirmed']), 2)
+df['DeathRate'] = round(100 * (df['Deaths'] / df['Confirmed']), 2)
 # df2['DeathRate'] = str(df2['DeathRate'])
+df['EstimatedCases'] = 8 * df['Confirmed']
+df['ActualLethality'] = round(100 * (df['Deaths'] / df['EstimatedCases']), 2)
 
-df2['Sum'] = df2.sum(axis=0)
+df = df[['Last_Update', 'Combined_Key', 'Confirmed', 'Deaths', 'DeathRate', 'EstimatedCases', 'ActualLethality']]
 
-df2 = df2[['Last_Update', 'Combined_Key', 'Confirmed', 'Deaths', 'DeathRate']]
+df.columns = ['Updated', 'City, State, Country', 'Confirmed', 'Deaths', '% Lethality', 'Estimated', '% Estimated Lethality']
 
-df2.columns = ['Updated', 'City, State, Country', 'Confirmed Cases', 'Deaths', '% Lethality']
+print(df.to_string(index=False))
 
-print(df2.to_string(index=False))
-
-df3 = df2.sum(axis=0)
-print(df3)
+# df2 = df
+df2 = df.sum(axis=0)
+# df2 = df2.loc['Confirmed', 'Deaths', 'DeathRate', 'EstimatedCases', 'ActualLethality']
+# df2 = df2[['Confirmed', 'Deaths', 'DeathRate']]
+df2['% Lethality'] = df2['% Lethality'] / len(df)
+df2['% Estimated Lethality'] = df2['% Estimated Lethality'] / len(df)
+print(df2.to_string(index=True))
